132d2b8bd3cc2b389ce9cf07965c809b34511f14
Sheldon Jackson <Sheldon.ej@gmail.com>
2023-08-19 09:33:53 -0400

    Version 0.0.2
    Updating Home Page and Theme Name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import logo from './assets/logo.svg';
-import styles from './App.module.css';
 
 function App() {
   return (
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
 import './index.css';
+import { render } from 'solid-js/web';
 import App from './App';
 
 const root = document.getElementById('root');
